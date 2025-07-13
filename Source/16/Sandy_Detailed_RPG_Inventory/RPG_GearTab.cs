9ec44ad97143e2ff12fff2d1aebffa3f9d6c6f30
p.tychinin <catgirlfighter@gmail.com>
2025-07-13 15:42:53 +0300

    Revert "Saw the error again, another try. Merging seems to be having problem."
    This reverts commit 9a4bff8a7f07d2260b7b10f5d39585435b7deace.

--- Source/16/Sandy_Detailed_RPG_Inventory/RPG_GearTab.cs
+++ Source/16/Sandy_Detailed_RPG_Inventory/RPG_GearTab.cs
@@ -902,8 +902,7 @@ namespace Sandy_Detailed_RPG_Inventory
         protected void DrawInventory(IEnumerable<Thing> list, string title, Rect viewRect, ref float num, bool inventory = false)
         {
             Widgets.ListSeparator(ref num, viewRect.width, title.Translate());
-            var itemList = list.ToList();
-            foreach (var item in itemList) DrawThingRow(ref num, viewRect.width, item, inventory);
+            foreach (var item in list.ToList()) DrawThingRow(ref num, viewRect.width, item, inventory);
         }
 
         protected virtual void DrawStats1(ref float top, float left)
